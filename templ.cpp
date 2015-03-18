a9305d41ea3973313c78ef0f1977a767f8f755d9
sharad <sharadsingh8@gmail.com>
2015-03-18 13:01:01 +0530

    first program for git

---
+++ templ.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<algorithm>
+#include<iostream>
+#include<vector>
+#include<map>
+#include<set>
+#include<string>
+#include<stack>
+#include<queue>
+
+typedef long long LL ;
+using namespace  std ;
+
+
+int main(){
+	return 0 ;
+}
